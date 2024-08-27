d7073ee1a5d108287aaa3b52e844a27324270e0b
Kayce Basques <kaycebasques@gmail.com>
2024-08-26 21:19:04 -0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
+# system
+
+sudo apt update -y
+sudo apt full-upgrade -y
+
+# apps
+
+sudo apt install vim -y
+
+# git
+
 read -p "Email: " EMAIL
 read -p "First name: " FIRST_NAME
 read -p "Last name: " LAST_NAME
@@ -6,17 +17,11 @@ git config --global user.email $EMAIL
 git config --global user.name "$FIRST_NAME $LAST_NAME"
 git config --global core.editor "vim"
 
-if [ ! -d ~/.ssh ]; then
-    echo "~/.ssh not found. creating now..."
-    # ssh-keygen -t ed25519 -C $EMAIL
-    # eval "$(ssh-agent -s)"
-    # ssh-add ~/.ssh/id_ed25519
-    # try ed25519-sk if ecdsa-sk doesn't work
-    ssh-keygen -t ecdsa-sk -C $EMAIL
+if [ ! -f ~/.ssh/ed25519.pub ]; then
+    ssh-keygen -t ed25519 -C $EMAIL
     eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_ecdsa-sk
-    cat ~/.ssh/id_ecdsa_sk.pub
-    echo "add the key to https://github.com/settings/keys"
-else
-    echo "~/.ssh already exists. skipping keygen..."
+    ssh-add ~/.ssh/id_ed25519
 fi
+
+cat ~/.ssh/id_ed25519.pub
+echo "add key to https://github.com/settings/ssh/new"
