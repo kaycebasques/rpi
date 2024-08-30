21ab6615a94a2059d6c2c080f79b3dda325ac9fe
Kayce Basques <kaycebasques@gmail.com>
2024-08-29 18:55:28 -0700

    add pico and bash setup

--- setup.sh
+++ setup.sh
@@ -1,29 +1,31 @@
 # system
-
 sudo apt update -y
 sudo apt full-upgrade -y
 
-# apps
+# bash
+echo "source ~/rpi/.bashrc" > ~/.bashrc
+source ~/.bashrc
 
+# apps
 sudo apt install vim -y
 
 # git
-
-read -p "Email: " EMAIL
-read -p "First name: " FIRST_NAME
-read -p "Last name: " LAST_NAME
-
-git config --global user.email $EMAIL
-git config --global user.name "$FIRST_NAME $LAST_NAME"
-git config --global core.editor "vim"
-
-if [ ! -f ~/.ssh/ed25519.pub ]; then
-    ssh-keygen -t ed25519 -C $EMAIL
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_ed25519
+NAME=$(git config --global user.name)
+if [ $NAME != "Kayce Basques" ]; then
+    read -p "Email: " EMAIL
+    git config --global user.email $EMAIL
+    git config --global user.name "Kayce Basques"
+    git config --global core.editor "vim"
+    if [ ! -f ~/.ssh/ed25519.pub ]; then
+        ssh-keygen -t ed25519 -C $EMAIL
+        eval "$(ssh-agent -s)"
+        ssh-add ~/.ssh/id_ed25519
+        cat ~/.ssh/id_ed25519.pub
+        echo "add key to https://github.com/settings/ssh/new"
+        git remote remove origin
+        git remote add origin git@github.com:kaycebasques/rpi.git
+    fi
 fi
-cat ~/.ssh/id_ed25519.pub
-echo "add key to https://github.com/settings/ssh/new"
 
-git remote remove origin
-git remote add origin git@github.com:kaycebasques/rpi.git
+# pico
+source pico.sh
